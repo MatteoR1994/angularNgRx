bcd0c2a40ed5d1f1f30335b069a9cfe9256401e9
MatteoR1994 <matteo.rinaldi1893@gmail.com>
2022-04-06 09:51:32 +0200

    Update counter.actions.ts

--- src/app/counter.actions.ts
+++ src/app/counter.actions.ts
@@ -1,5 +1,5 @@
 import { createAction } from "@ngrx/store";
 
-export const increment = createAction('[Counter Component] Increment');
+export const increment = createAction('[Counter Component] Increment'); // Tra le [] va il componente di riferimento, e dopo è il comando vero e proprio che verrà lanciato.
 export const decrement = createAction('[Counter Component] Decrement');
 export const reset = createAction('[Counter Component] Reset');
